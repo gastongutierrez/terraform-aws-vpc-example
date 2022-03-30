2b353baf448cf6797cef6e66e4c765582c89fe88
gaston <gastongutierrez@gmail.com>
2022-03-30 09:43:00 -0400

    added public instances outbound sg rules

--- vpc_dev.tf
+++ vpc_dev.tf
@@ -115,7 +115,31 @@ resource "aws_security_group" "dev-public-sg" {
         protocol = "tcp"
         cidr_blocks = [var.dev_private_subnet_cidr]
     }
-    
+
+    egress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "DNS"
+        from_port = 53
+        to_port = 53
+        protocol = "udp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = {
         Name = "dev-public-sg"
     }
--- vpc_prod.tf
+++ vpc_prod.tf
@@ -116,6 +116,30 @@ resource "aws_security_group" "prod-public-sg" {
         cidr_blocks = [var.prod_private_subnet_cidr]
     }
 
+    egress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "DNS"
+        from_port = 53
+        to_port = 53
+        protocol = "udp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = {
         Name = "prod-public-sg"
     }
